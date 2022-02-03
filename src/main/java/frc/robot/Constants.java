13fcb7db19c46af89bd1dbe757c4ba72876aadbd
EloBenccc <dylewskipoczta@gmail.com>
2022-02-03 19:12:59 +0100

    Intake fix

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -48,7 +48,7 @@ public final class Constants {
 
     public static class Intake {
 
-        // NOT CURRENT VALUES
+        // NOT CURRENT
         public static final double ROBOT_INTAKE_WHEEL_CIRCUIT = 100; // [mm]
         public static final double ENCODER_TICK_RATE = 256;
         public static final double ENCODER_MIN_RATE = 10; // Configures the encoder to consider itself stopped when it is bellow min rate
